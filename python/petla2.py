7a41124b093a573366dfc06684b55adb5a3b88a7
maciek202 <maciej.kanar@lo1.sandomierz.pl>
2019-11-20 11:41:19 +0100

    pętla while

---
+++ python/petla2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def sumuj_liczby():
+    """
+    Funkcja pobiera liczby od użytkownika dopóki ich suma
+     nie przekroczy wartości 75. Na koniec drukuje sume liczb.
+    """
+    suma = 0
+    i = 0
+    while suma < 75:
+        liczba = int(input("Podaj liczbę: "))
+        if i == 0 and liczba > 75:
+            print("Nie chcesz za dużo?")
+            continue
+        suma = suma + liczba 
+        i = i + 1
+    
+    print(suma)
+     
+
+def main(args):
+    sumuj_liczby()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ python/ćwiczenie 2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(args):
+    
+    n = int(input("Podaj liczbę n: "))
+    m = int(input("Podaj liczbę m: "))
+    for list(range(n,m))
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
