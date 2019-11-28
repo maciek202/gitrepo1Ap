6984d1031fcfb8c0fedb57f7103b95eeceaa6d71
maciek202 <maciej.kanar@lo1.sandomierz.pl>
2019-11-28 12:33:48 +0100

    c++

--- c++/równanie.cpp
+++ c++/równanie.cpp
@@ -12,6 +12,24 @@ using namespace std;
 int main(int argc, char **argv)
 {
     
+    int a;
+    int b;
+    float x;
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "Nieskończenie wiele rozwiązań";
+            } else {
+                cout << "równanie sprzeczne";
+            }
+            
+    } else {
+        x = -b/(float)a;
+        cout << x;
+    }
 	return 0;
 }
 
