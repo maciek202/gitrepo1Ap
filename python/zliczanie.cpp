ae17235c982b987a1f25809dbff1ec9cc0895480
maciek202 <maciej.kanar@lo1.sandomierz.pl>
2019-12-18 11:01:57 +0100

    test

---
+++ python/zliczanie.cpp
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    a = int(input("Podaj liczbe: "))
+    while a > 0:
+        if a % 5 and a% 7:
+            
+    
+    
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
