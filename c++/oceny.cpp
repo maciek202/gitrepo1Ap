91e365c17a8ceaaaf17b9078156d5947665c126f
maciek202 <maciej.kanar@lo1.sandomierz.pl>
2020-01-09 11:52:25 +0100

    oceny zadanie

---
+++ c++/oceny.cpp
@@ -0,0 +1,35 @@
+/*
+ * oceny.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	float ilosc,ocena,srednia,suma;
+    ocena = 0;
+    cout << "Ile jest ocen: ";
+    cin >> ilosc;
+    while (ilosc < 1){
+        cout << "Ile jest ocen: ";
+        cin >> ilosc;
+        }
+    for(int i = 0; i < ilosc; i = i + 1){
+        cout << "Podaj ocenę: ";
+        cin >> ocena;
+        while (ocena < 1 or ocena > 6){
+            cout << "Podaj dobrą ocenę: ";
+            cin >> ocena;
+            
+        }
+        suma = suma + ocena;
+        }
+    srednia = suma / ilosc;
+    cout << "Średnia: ";
+    cout << srednia;
+
+	return 0;
+}
+
